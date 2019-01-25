112d182538394c06153edb22afdfd7f70709c0d0
朱彦霖 <zhuyanlin@renrenche.com>
2019-01-25 17:20:07 +0800

    waiting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import TodoList from './views/TodoList';
+import TodoList from './TodoList'
 
-// PWA
-import * as serviceWorker from './serviceWorker';
-
-import RouterMap from './RouterMap'
-
-// JSX语法中，如果我们要使用自己创建的组件组件名必须以大写字母开头如TodoList
-ReactDOM.render(<RouterMap />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<TodoList />, document.getElementById('root'));
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,6 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore } from 'redux'
 import reducer from './reducer'
-// import thunk from 'redux-thunk'
-// import createSagaMiddleware from 'redux-saga'
-// import sagas from './sagas'
 
-// const sagaMiddleware = createSagaMiddleware()
+const store = createStore()
 
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
-
-const enhancer = composeEnhancers(
-  // applyMiddleware(sagaMiddleware)
-);
-
-const store = createStore(
-  reducer,
-  enhancer
-);
-
-// sagaMiddleware.run(sagas)
-
-export default store;
+export default store
